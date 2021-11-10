c70fab68a9c43d2727c7e498bb83ac75eaf5850f
jeminasy <jemina.sy.iics@ust.edu.ph>
2021-11-10 22:51:59 +0800

    'aaaaaaa'

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const server = require('./server');
 const express = require('express');
-// const jsreport = require('jsreport');
 
-const jsreport = require('@jsreport/jsreport-core')();
+const client = require("@jsreport/nodejs-client")('http://localhost:5488', 'admin', 'password');
+const jsrender = require('jsrender');
 
-// jsreport.use(require('@jsreport/jsreport-chrome-pdf')());
-// jsreport.use(require('@jsreport/jsreport-handlebars')());
+const myData = require('./data');
 
 const app = express();
 app.listen(3000, () => {
@@ -16,40 +15,19 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-// app.get('/report', (req, res) => {
-//     jsreport.render({
-//         template: {name: "temp1"}
-//     }).then(out => {
-//         out.stream.pipe(res);
-//     }).catch(err => {
-//         res.send("Error: " + err);
-//     });
-// });
-
-app.get('/report', async (req, res) => {
-    jsreport.init();
-    jsreport.render({
-        template: {shortid: "SywrNY7wf"}
-    }).then(out => {
-        out.stream.pipe(res);
-    }).catch(err => {
-        res.send('Error: ' + err);
-    });
+app.get('/name', (req, res) => {
+    var temp = jsrender.templates('Name: {{:name}}<br />');
+    var html = temp.render({name: "Jim"});
+    res.send(html);
 });
 
-// app.get('/report', async (req, res) => {
-//     const result = await jsreport.render({
-//         template: {shortid: 'SywrNY7wf'}
-//     });
-//     res.send(result.content);
-// });
-
-// const proxy = require('jsreport-proxy');
-// app.get('/report', (req, res, done) => {
-//     proxy.render({
-//         template: {shortid: "SywrNY7wf"}
-//     }, function (err, res) {
-//         done();
-//     });
-// })
-
+app.get('/report', (req, res) => {
+    client.render({
+        template: {name: "temp1"},
+        data: myData
+    }).then((response) => {
+        response.pipe(res);
+    }).catch((err) => {
+        res.send("Error: " + err);
+    });
+});
